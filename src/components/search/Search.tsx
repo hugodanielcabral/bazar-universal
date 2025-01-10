5b2e8f733d3a8d90c3308f96bf24621bac1e7fc8
Daniel Cabral <danicabral15@outlook.com>
2025-01-10 10:49:05 -0300

    Implement search functionality with input handling and navigation

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,5 +1,42 @@
+import { useRef, useState } from "react";
+import { useNavigate } from "react-router";
+
 export const Search = () => {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!search) return;
+
+    navigate(`items/?search=${search}`);
+  };
+
   return (
-    <div>Search</div>
-  )
-}
+    <form onSubmit={handleSubmit} className="flex flex-col gap-2">
+      <input
+        type="search"
+        name="search"
+        id="search"
+        placeholder="laptops, smartphones, ..."
+        autoComplete="off"
+        className="p-2 rounded-md text-black focus:border-yellow-500 focus:border"
+        value={search}
+        onChange={handleChange}
+        ref={inputRef}
+      />
+      <button
+        type="submit"
+        disabled={!search}
+        className="p-2 bg-yellow-600 hover:bg-yellow-700 text-white text-xl rounded-md disabled:bg-opacity-25 disabled:text-gray-400"
+      >
+        Buscar
+      </button>
+    </form>
+  );
+};
